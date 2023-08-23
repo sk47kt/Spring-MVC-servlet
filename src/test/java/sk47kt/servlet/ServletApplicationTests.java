557fe05281be867357b167c862b9bcfe9337c9dd
sk47kt <sk47kt@naver.com>
2023-08-23 19:50:20 +0900

    Initial commit

---
+++ src/test/java/sk47kt/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package sk47kt.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
