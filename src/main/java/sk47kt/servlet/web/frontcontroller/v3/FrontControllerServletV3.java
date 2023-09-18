03834ffd0d53e9806328d5fb6d34893b43b0acfa
sk47kt <sk47kt@naver.com>
2023-09-18 17:30:04 +0900

    adapter pattern

--- src/main/java/sk47kt/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
+++ src/main/java/sk47kt/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -48,11 +48,8 @@ public class FrontControllerServletV3 extends HttpServlet {
 
     private static Map<String, String> createParamMap(HttpServletRequest request) {
         Map<String,String> paramMap = new HashMap<>();
-
         request.getParameterNames().asIterator()
                 .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
-
-
         return paramMap;
     }
 
--- src/main/java/sk47kt/servlet/web/servletmvc/MvcMemberFormServlet.java
+++ src/main/java/sk47kt/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class MvcMemberFormServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String viewPath = "/WEB-INF/views/new-form.jsp";
+        String viewPath = "/WEB-INF/views/test.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
